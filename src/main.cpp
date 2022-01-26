2acf7ef6648d3e48f869dfcf96294edae43ef651
hicham-vv <77798347+hicham-vv@users.noreply.github.com>
2022-01-26 15:04:03 +0100

    dummy

--- src/main.cpp
+++ src/main.cpp
@@ -1,467 +1,218 @@
-/*********
-  Modified from the examples of the Arduino LoRa library
-  More resources: https://randomnerdtutorials.com
-*********/
 
-#include <SPI.h>
-#include <LoRa.h>
-#include <math.h>
-
-#include <WiFi.h>
-#include <HTTPClient.h>
+//*********************Sender***************//
+#include <stdio.h>
+#include <stdlib.h>
 
 
-const char * ssid = "Orange-80C3";
-// const char * ssid = "Redmi 9T";
+#include <Layer1_LoRa.h>
+#include <LoRaLayer2.h>
 
-const char * password = "0GEYH04G5A2";
-// const char * password = "luffy123";
+#define LORA_CS 4
+#define LORA_RST 22
+#define LORA_IRQ 21
+// #define LORA_FREQ 915E6
+#define LORA_FREQ 868E6
+#define LED 2
+#define TX_POWER 20
 
-
-
-byte Buffer[5];
-unsigned BytesRead = 0;
-unsigned DistanceInMM = 0;
+// #define Sender
+#define Receiver
+// #define GetMacAdress
 
 
 
-unsigned long currentMillis = 0;
-unsigned long previousMillis = 0;
-int d=0;
-int sum=1;
-String Index ="T";
+#ifdef Sender
 
-bool vDist = false;
+char MAC[9] = "bf8660c0";
 
-bool confirm=false;
+// uint8_t LOCAL_ADDRESS[ADDR_LENGTH] = {0xbf, 0x86, 0x61, 0x4c};
+uint8_t RECEIVER[ADDR_LENGTH] = {0xbf, 0x86, 0x61, 0x08};
 
-uint8_t distance=0;
-
-uint8_t Dr=200; 
-int8_t niveauR=0;
-
-uint8_t distanceRef=123;
+ // bf:86:61:08 Receiver MAC Adress 
 
 
+Layer1Class *Layer1;
+LL2Class *LL2;
 
-
-const char* serverName = "http://141.94.71.45:8081/datasnap/rest/Tdata/rep";
-
-
-//define the pins used by the transceiver module
-#define ss 4
-#define rst 22
-#define dio0 21
-
-#define pin3V 26
-
-#define pin5V 25
-
-#define Led 2
-
-
-
-
-
-// #define Master
-#define Slave
-
-
-
-
-// #define HTTPsend
-// #define Sensor
-
-
-
-
-
-
-#ifdef Slave
-
-
-
-
+int counter = 0;
+long lastTransmit;
 
 void setup() {
-  //initialize Serial Monitor
   Serial.begin(115200);
-  while (!Serial);
-  Serial2.begin(9600);
-  while (!Serial2);
-  
+  while(!Serial);
+  delay(2000);
+  pinMode(26, OUTPUT); //EN 3.3V
+  digitalWrite(26, HIGH);
+
+  SPI.begin(18, 19, 23, 4); // 
+
+  Serial.println("* Initializing LoRa...");
   Serial.println("LoRa Sender");
 
-  
-  pinMode(pin3V,OUTPUT);
-  digitalWrite(pin3V,HIGH);
-  pinMode(pin5V,OUTPUT);
-  digitalWrite(pin5V,HIGH);
-
-
-
-  //setup LoRa transceiver module
-  LoRa.setPins(ss, rst, dio0);
-  
-  //replace the LoRa.begin(---E-) argument with your location's frequency 
-  //433E6 for Asia
-  //866E6 for Europe
-  //915E6 for North America
-  while (!LoRa.begin(866E6)) {
-    Serial.print(".");
-    delay(500);
+  Layer1 = new Layer1Class();
+  Layer1->setPins(LORA_CS, LORA_RST, LORA_IRQ);
+  Layer1->setTxPower(TX_POWER);
+  Layer1->setLoRaFrequency(LORA_FREQ);
+  if (Layer1->init())
+  {
+    Serial.println(" --> Layer1 initialized");
+    LL2 = new LL2Class(Layer1); // initialize Layer2
+    LL2->setLocalAddress(MAC); // this should either be randomized or set using the wifi mac address
+    LL2->setInterval(10000); // set to zero to disable routing packets
+    if (LL2->init() == 0){
+      Serial.println(" --> LoRaLayer2 initialized");
+    }
+    else{
+      Serial.println(" --> Failed to initialize LoRaLayer2");
+    }
   }
-   // Change sync word (0xF3) to match the receiver
-  // The sync word assures you don't get LoRa messages from other LoRa transceivers
-  // ranges from 0-0xFF
-  LoRa.setSyncWord(0xFF);
-  LoRa.setSpreadingFactor(7);
-  LoRa.setSignalBandwidth(125E3);
-  Serial.println("LoRa Initializing OK!");
+  else
+  {
+    Serial.println(" --> Failed to initialize LoRa");
+  }
+  lastTransmit = Layer1Class::getTime();
 }
 
 void loop() {
+  LL2->daemon();
+  int msglen = 0;
+  int datagramsize = 0;
 
-  unsigned prevmillis=millis();
-  while((millis()-prevmillis)<2000){
-    if (Serial2.available())
-    {
-      // Put the character in the buffer
-      Buffer[BytesRead++] = Serial2.read();
-      // Validate the input so far:
-      switch (BytesRead)
-      {
-        case 1:  // Start character
-          if (Buffer[0] != 0xFF) // Invalid start character
-          {
-            BytesRead = 0; // Start over
-          }
-          break;
+  struct Datagram datagram; 
+  if (Layer1Class::getTime() - lastTransmit >= 5000){
+    msglen = sprintf((char*)datagram.message, "%s,%i", "Lora", counter);
 
-        case 4:  // Checksum
-          byte sum;  // DO NOT INITIALIZE LOCAL VARIABLES IN A 'case' CLAUSE.
-          sum = Buffer[0] + Buffer[1] + Buffer[2];
-          if (sum != Buffer[3])
-          {
-            // Serial.println("Invalid checksum: 0xFF + 0x");
-            BytesRead = 0; // Start over
-          }
-          break;
-        case 5: // End character
-          if (Buffer[4] != 0xFF)
-          {
-            BytesRead = 0; // Start over
-          }
-          break;
-      }
-    }
-    if (BytesRead == 5)
-    {
-      DistanceInMM = Buffer[1] * 256 + Buffer[2];
-      BytesRead = 0; // Look for a new start next time
-      if (DistanceInMM > 30)
-      {
-
-        distance= DistanceInMM / 10;
-        Serial.print("distance=");
-        Serial.print(distance);
-        Serial.println("cm");
-
-      }
-      else
-      {
-        Serial.println("distance=3cm");
-        distance=3;
-      }
-      
-      if(abs(Dr-distance)>6){
-        confirm=true;
-        Dr=distance;
-      }
-      if(confirm){
-        float a=distanceRef-distance;
-        a=a/distanceRef;
-        a=a*100; 
-        niveauR=int(a);
-        if(niveauR<0){
-          niveauR=0;
-        }
-        if(niveauR>=97){
-          niveauR=100;
-        }
-
-        Serial.print("Niveau de remplissage=");
-        Serial.print(niveauR);
-        Serial.println("%");
-        vDist=true;
-        confirm=false;
-      }
-
-
-
-    }
+    memcpy(datagram.destination, RECEIVER, ADDR_LENGTH);
+    datagram.type = 's'; // can be anything, but 's' for 'sensor'
+    Serial.print("DATALen= ");
+    Serial.println(msglen);
+    datagramsize = msglen + HEADER_LENGTH;
+    Serial.println(datagramsize);
+    LL2->writeData(datagram, datagramsize);
+    counter++;
+    Serial.println((char*)datagram.message);
+    lastTransmit = Layer1Class::getTime();
+    Serial.println();
   }
-    if(vDist){
-      //Send LoRa packet to receiver
-      Serial.print("Let send Here the data : Niveau de remplissage = ");
-      Serial.println(niveauR);
-      LoRa.beginPacket();
-      String loradata="N4,";
-      loradata=loradata+niveauR;
-      LoRa.print(loradata);
-      LoRa.endPacket();
-      vDist=false;
-    }
-    
-    delay(5000);
-  }
+
+}
 
 #endif
 
-#ifdef Master
+
+
+
+#ifdef Receiver
+
+//********************Receiver*********************//
+#include <stdio.h>
+#include <stdlib.h>
+
+//LoRaLayer2
+#include <Layer1_LoRa.h>
+#include <LoRaLayer2.h>
+// #define LL2_DEBUG
+
+#define LORA_CS 4
+#define LORA_RST 22
+#define LORA_IRQ 21
+
+
+#define LORA_FREQ 868E6
+#define LED 2
+#define TX_POWER 20
+
+char MAC[9] = "bf866108";
+// uint8_t LOCAL_ADDRESS[ADDR_LENGTH] = {0xbf, 0x86, 0x60, 0xb8};
+// GATEWAY is the receiver 
+// uint8_t SENDER[ADDR_LENGTH] = {0xc0, 0xd3, 0xf0, 0x0d};
+
+Layer1Class *Layer1;
+LL2Class *LL2;
+
+int counter = 0;
 
 void setup() {
-  //initialize Serial Monitor
   Serial.begin(115200);
-  while (!Serial);
+  while(!Serial);
+  pinMode(26, OUTPUT); //EN 3.3V
+
+  digitalWrite(26, HIGH);
+
+
+  SPI.begin(18, 19, 23, 4); // not needed for ttgo-lora32-v1
+  Serial.println("* Initializing LoRa...");
   Serial.println("LoRa Receiver");
 
-  pinMode(Led,OUTPUT);
-
-
-  pinMode(pin3V,OUTPUT);
-  digitalWrite(pin3V,HIGH);
-  
-
-  pinMode(pin5V,OUTPUT);
-  digitalWrite(pin5V,HIGH);
-
-
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting");
-  unsigned long pmillis=millis();
-  while(WiFi.status() != WL_CONNECTED && (millis()-pmillis)<5000) {
-    delay(500);
-    Serial.print(".");
+  Layer1 = new Layer1Class();
+  Layer1->setPins(LORA_CS, LORA_RST, LORA_IRQ);
+  Layer1->setTxPower(TX_POWER);
+  Layer1->setLoRaFrequency(LORA_FREQ);
+  if (Layer1->init())
+  {
+    Serial.println(" --> Layer1 initialized");
+    LL2 = new LL2Class(Layer1); // initialize Layer2
+    LL2->setLocalAddress(MAC); // this should either be randomized or set using the wifi mac address
+    LL2->setInterval(1); // set to zero to disable routing packets
+    if (LL2->init() == 0){
+      Serial.println(" --> LoRaLayer2 initialized");
+    }
+    else{
+      Serial.println(" --> Failed to initialize LoRaLayer2");
+    }
   }
-  if((millis()-pmillis)>=5000){
-    Serial.print("Can't Connect to WiFi: ");
-    ESP.restart();
+  else
+  {
+    Serial.println(" --> Failed to initialize LoRa");
   }
-  digitalWrite(Led,HIGH);
-  
-
-  Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
-  
-  //setup LoRa transceiver module
-  LoRa.setPins(ss, rst, dio0);
-  
-  //replace the LoRa.begin(---E-) argument with your location's frequency 
-  //433E6 for Asia
-  //866E6 for Europe
-  //915E6 for North America
-  while (!LoRa.begin(866E6)) {
-    Serial.println(".");
-    delay(500);
-  }
-   // Change sync word (0xF3) to match the receiver
-  // The sync word assures you don't get LoRa messages from other LoRa transceivers
-  // ranges from 0-0xFF
-  LoRa.setSyncWord(0xFF);
-  LoRa.setSpreadingFactor(7);
-  LoRa.setSignalBandwidth(125E3);
-  Serial.println("LoRa Initializing OK!");
-  delay(500);
 }
 
 void loop() {
-  // try to parse packet
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    Serial.print("Received packet");
-    // read packet
-    while (LoRa.available()){
-      String LoRaData = LoRa.readString();
-      Serial.println(LoRaData);
-      String datatopost= "[{\"X\":\"";
-      datatopost=datatopost+LoRaData+"\"}]";
-      Serial.println(datatopost);
-      if(WiFi.status()== WL_CONNECTED){
-        WiFiClient client;
-        HTTPClient http;
-        // Your Domain name with URL path or IP address with path
-        http.begin(client, serverName);
 
-        http.addHeader("Content-Type", "application/json");
-        int httpResponseCode = http.POST(datatopost);
-        Serial.print("HTTP Response code: ");
-        Serial.println(httpResponseCode);
-        http.end();
-      }
-      else {
-        Serial.println("WiFi Disconnected");
-        ESP.restart();
-      }
-    }
+  char routes[256];
+  char neighbors[256];
+
+  LL2->daemon();
+
+  struct Packet packet = LL2->readData();
+  if(packet.totalLength > HEADER_LENGTH)
+  {
+    Serial.println(((char *)packet.datagram.message));
+
+    // LL2->getRoutingTable(routes);
+    // Serial.printf("%s", routes);
+
+    LL2->getNeighborTable(neighbors);
+    Serial.println(neighbors);
   }
 }
+
 #endif
 
 
-#ifdef Sensor
 
 
 
 
-byte Buffer[5];
-unsigned BytesRead = 0;
-unsigned DistanceInMM = 0;
+#ifdef GetMacAdress
 
-void setup()
-{
-  Serial.begin(115200);
-  while (!Serial);
-  Serial2.begin(9600);
-  while (!Serial2);
-  
-  Serial.println("LoRa Sender");
-
-  
-  pinMode(pin3V,OUTPUT);
-  digitalWrite(pin3V,HIGH);
-  pinMode(pin5V,OUTPUT);
-  digitalWrite(pin5V,HIGH);
-}
-
-void loop()
-{
-  if (Serial2.available())
-  {
-    // Put the character in the buffer
-    Buffer[BytesRead++] = Serial2.read();
-    // Validate the input so far:
-    switch (BytesRead)
-    {
-      case 1:  // Start character
-        if (Buffer[0] != 0xFF)
-        {
-          Serial.print("Invalid start character: ");
-          Serial.println(Buffer[0], HEX);
-          BytesRead = 0; // Start over
-        }
-        break;
-
-      case 4:  // Checksum
-        byte sum;  // DO NOT INITIALIZE LOCAL VARIABLES IN A 'case' CLAUSE.
-        sum = Buffer[0] + Buffer[1] + Buffer[2];
-        if (sum != Buffer[3])
-        {
-          Serial.print("Invalid checksum: 0xFF + 0x");
-          Serial.print(Buffer[1], HEX);
-          Serial.print(" + 0x");
-          Serial.print(Buffer[2], HEX);
-          Serial.print(" != 0x");
-          Serial.println(Buffer[3], HEX);
-          BytesRead = 0; // Start over
-        }
-        break;
-
-      case 5: // End character
-        if (Buffer[4] != 0xFF)
-        {
-          Serial.print("Invalid end character: ");
-          Serial.println(Buffer[4], HEX);
-          BytesRead = 0; // Start over
-        }
-        break;
-    }
-  }
-
-  if (BytesRead == 5)
-  {
-    DistanceInMM = Buffer[1] * 256 + Buffer[2];
-    BytesRead = 0; // Look for a new start next time
-
-    if (DistanceInMM > 30)
-    {
-      Serial.print("distance=");
-      Serial.print(DistanceInMM / 10.0, 1); // Convert to cm and show with 1 decimal place
-      Serial.println("cm");
-    }
-    else
-    {
-      Serial.println("Below the lower limit");
-    }
-  }
-
-  delay(5);
-}
+//***************GET MAC Address*******************//
+// #include <SPI.h>
+#ifdef ESP32
+  #include <WiFi.h>
+#else
+  #include <ESP8266WiFi.h>
 #endif
 
-#ifdef HTTPsend
-
-
-// the following variables are unsigned longs because the time, measured in
-// milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long lastTime = 0;
-// Timer set to 10 minutes (600000)
-//unsigned long timerDelay = 600000;
-// Set timer to 5 seconds (5000)
-unsigned long timerDelay = 5000;
-
-void setup() {
+void setup(){
   Serial.begin(115200);
-
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
- 
-  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+  Serial.println();
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
 }
 
-void loop() {
-  //Send an HTTP POST request every 10 minutes
-    //Check WiFi connection status
-    if(WiFi.status()== WL_CONNECTED){
-      WiFiClient client;
-      HTTPClient http;
-    
-      // Your Domain name with URL path or IP address with path
-      http.begin(client, serverName);
+void loop(){
 
-      // Specify content-type header
-      // http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-      // // Data to send with HTTP POST
-      // String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=BME280&value1=24.25&value2=49.54&value3=1005.14";           
-      // // Send HTTP POST request
-      // int httpResponseCode = http.POST(httpRequestData);
-      
-      // If you need an HTTP request with a content type: application/json, use the following:
-      http.addHeader("Content-Type", "application/json");
-      int httpResponseCode = http.POST("{\"Hello Rachid \"}");
-
-      // If you need an HTTP request with a content type: text/plain
-      //http.addHeader("Content-Type", "text/plain");
-      //int httpResponseCode = http.POST("Hello, World!");
-     
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
-        
-      // Free resources
-      http.end();
-      delay(2000);
-    }
-    else {
-      Serial.println("WiFi Disconnected");
-    }
 }
 
 #endif
